dee47d3b4100e77af5840c5dfd39b50eaba22460
Tsuki <Tsuki@DESKTOP-RU0UJKI>
2018-12-11 19:54:43 +0100

    cositas de css #1

--- views/layout.php
+++ views/layout.php
@@ -4,16 +4,23 @@
     </head>
     <link rel="stylesheet" type="text/css" href="http://localhost/m14_proyecto/estil.css" />
     <body>
-        <header>
-            <a href='<?php echo constant('URL');?>'>Perfil</a>
-            <a href='<?php echo constant('URL');?>usuari/index'>Usuaris</a>
-            <a href='<?php echo constant('URL');?>personal/index'>Perosonal</a>
-        </header>
-        <main>
-            <?php require_once('routes.php'); ?>
-        </main>
-        <footer>
-            Copyright
-        </footer>
+        <div class="container">
+            <header>
+                <div class="header"> 
+                    
+                    <div class="nav">
+                        <a href='<?php echo constant('URL');?>'>Perfil</a>
+                        <a href='<?php echo constant('URL');?>usuari/index'>Usuaris</a>
+                        <a href='<?php echo constant('URL');?>personal/index'>Perosonal</a>
+                    </div>
+                </div>
+            </header>
+            <main>
+                <?php require_once('routes.php'); ?>
+            </main>
+            <footer>
+                Copyright
+            </footer>
+        </div>
     </body>
 </html>
