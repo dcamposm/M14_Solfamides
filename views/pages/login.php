0dc7ff6dd3e3541f6f1bdab66c3c0358bf2b9043
DieCM <didaccampos@gmail.com>
2018-12-11 17:02:45 +0100

    BASE LOGIN 2

--- views/pages/login.php
+++ views/pages/login.php
@@ -4,6 +4,8 @@
     <input type='text' name='user'/> 
     <p>Password<p>
     <input type='text' name='pass'/>
-    <button type="submit">Click</button>
+    <br><label><input type="checkbox" id="cbox1" value="remember"> Remember username</label>
+    <br><a>Forgotten your username or password?</a>
+    <br><button type="submit">Click</button><!--Cambiar el boton por una imagen(Mirar plantillas)-->
 </form>
 
