b78d9b3789822de386e8da5f37651f1895f84a32
Tsuki <Tsuki@DESKTOP-RU0UJKI>
2018-12-12 01:49:04 +0100

    CSS("login");

--- views/pages/login.php
+++ views/pages/login.php
@@ -1,11 +1,22 @@
-<p>LOGIN<p>
-<form action="<?php echo constant('URL');?>usuari/login" method="post" enctype="multipart/form-data"> 
-    <p>Username<p>
-    <input type='text' name='user'/> 
-    <p>Password<p>
-    <input type='text' name='pass'/>
-    <br><label><input type="checkbox" id="check" value="rem"> Remember username</label>
-    <br><a href='<?php echo constant('URL');?>pages/rem'>Forgotten your username or password?</a>
-    <br><button type="submit">Click</button><!--Cambiar el boton por una imagen(Mirar plantillas)-->
-</form>
+<h1>LOGIN</h1>
+<div class="spacing_login">
+    <form action="<?php echo constant('URL');?>usuari/login" method="post" enctype="multipart/form-data">
+        <div>
+            <div>
+                <p>Username<p>
+                <input type='text' name='user'/>
+            </div>
+
+            <div>
+                <p>Password<p>
+                <input type='text' name='pass'/>
+            </div>
+            <label><input type="checkbox" id="check" value="rem"> Remember username</label>
+        </div>
+        <div>
+            <a href='<?php echo constant('URL');?>pages/rem'>Forgotten your username or password?</a>
+        </div>
+        <button type="submit">Click</button><!--Cambiar el boton por una imagen(Mirar plantillas)-->
+    </form>
+</div>
 
